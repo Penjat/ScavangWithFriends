fa43c4c3d0c8c2c453b47f437b2a3301fefe8a06
Penjat <theSpencerSymington@Gmail.com>
2021-05-02 20:28:25 -0700

    Paging scrollview.

--- ScavangWithFriends/SingleGameView.swift
+++ ScavangWithFriends/SingleGameView.swift
@@ -1,13 +1,26 @@
 import SwiftUI
 
 struct SingleGameView: View {
-    var body: some View {
-        Text("Single Game")//.navigationBarHidden(true)
-    }
+	@State private var selectedTab = 0
+	let things = ["elephant", "pig", "chair", "ball", "cat", "rabbit"]
+	var body: some View {
+
+		VStack() {
+			Text("\(selectedTab)")
+			Spacer()
+			TabView(selection: $selectedTab){
+				ForEach(0..<things.count) { index in
+					Text(things[index]).tag(index)
+				}
+			}
+			.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+			.frame(width: UIScreen.main.bounds.width, height: 200, alignment: .bottom)
+		}
+	}
 }
 
 struct SingleGameView_Previews: PreviewProvider {
-    static var previews: some View {
-        SingleGameView()
-    }
+	static var previews: some View {
+		SingleGameView()
+	}
 }
